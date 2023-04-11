cc6c2f29360e0195a89e5682cd70eb4c330a7368
david820428 <david820428@yahoo.com.tw>
2023-04-11 13:03:23 +0800

    login page successful

--- app.js
+++ app.js
@@ -22,10 +22,6 @@ let weatherDescription = "";
 let imageURL = "";
 let temp = "";
 
-
-
-
-
 app.get("/", function(req, res) {
   let today = new Date();
   let currentDay = today.getDay();
@@ -40,10 +36,54 @@ app.get("/", function(req, res) {
   });
 });
 
+app.post("/", function(req, res) {
+  console.log(req.body.acnm);
+  console.log(req.body.pwd);
+
+  let acc_name = req.body.acnm;
+  let paswd = req.body.pwd;
+  var identify = false;
+
+  const client = new Client({
+    user: 'mydb_ab3v_user',
+    host: 'dpg-cgnng2ou9tun42st144g-a.singapore-postgres.render.com',
+    database: 'mydb_ab3v',
+    password: 'NszA3WUQwKvA9pA3Uu5cerIiylNL7Pkp',
+    port: 5432,
+    ssl: true
+  });
+
+  client.connect(function(err) {
+    console.log(err);
+  });
+
+  client.query(`SELECT EXISTS ( SELECT * FROM account WHERE name = $1 AND password = $2)`, [acc_name, paswd], (err, res) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log("login success");
+      identify = res.rows[0].exists;
+      console.log(identify);
+    };
+  });
+
+  setTimeout(() => {
+    if (identify) {
+      res.redirect("/CityWeather");
+    } else {      
+      res.redirect("/");
+    }
+    console.log("Delayed for 3 second.");
+  }, 3000);
+
+
+});
+
 app.get("/signup", function(req, res) {
 
   res.render("signup");
 });
+
 
 app.get("/CityWeather", function(req, res) {
 
@@ -92,31 +132,34 @@ app.post("/signup", async (req, res) => {
 
   const client = new Client({
     user: 'mydb_ab3v_user',
-    host: 'dpg-cgnng2ou9tun42st144g-a',
+    host: 'dpg-cgnng2ou9tun42st144g-a.singapore-postgres.render.com',
     database: 'mydb_ab3v',
     password: 'NszA3WUQwKvA9pA3Uu5cerIiylNL7Pkp',
     port: 5432,
     ssl: true
   });
+
   client.connect(function(err) {
     console.log(err);
   });
 
-  const insertQuery = `INSERT INTO "accountList"."accountList" (account_id, account_name, password, email) VALUES (3, $1, $2, $3)`;
-
-  // define the data to be inserted
-  const data = [acc_name, paswd, emlads];
-
-  // execute the insert query using the pool
-  client.query(insertQuery, data, (err, res) => {
+  client.query(`INSERT INTO account (name, password, email) VALUES ($1, $2, $3)`, [acc_name, paswd, emlads], (err, res) => {
     if (err) {
-      console.error(err);
+      console.log(err.message);
     } else {
-      console.log(`Successfully inserted ${res.rowCount} row(s)`);
+      console.log("insert success");
     };
   });
-  // release the pool to free up resources
-  client.end();
+
+  // execute the insert query using the pool
+  client.query(`SELECT * from account`, (err, res) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log(res.rows);
+    };
+    client.end();
+  });
 
 
   console.log("succeed");
